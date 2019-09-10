dd94c334730510d7a5a77f206745ff590096af40
Scabandari <ryan3nichols@gmail.com>
2019-09-10 12:21:57 -0400

    Closes #4 HOC passing height and width down to child

--- src/components/GameSession/GameSession.jsx
+++ src/components/GameSession/GameSession.jsx
@@ -2,15 +2,16 @@ import React, { useState } from 'react';
 import { GameBoard } from '../index';
 import ControlPanel from './ControlPanel';
 import { SoundPlayer } from '../../containers';
-//import { Button } from 'semantic-ui-react';
 import soundfile from '../../assets/a.mp3';
 import PropTypes from 'prop-types';
 import './GameSession.scss';
+import DisplayScreenSize from '../DisplayScreenSize';
 
 const GameSession = props => {
   return (
     <div className='site-content'>
       <ControlPanel />
+      <DisplayScreenSize />     
       <GameBoard />
     </div>
   );
---
+++ src/components/hoc/passScreenSize.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+
+export default ChildComponent => {
+    class PassScreenSize extends Component {
+        // ref: https://stackoverflow.com/questions/36862334/get-viewport-window-height-in-reactjs
+        constructor(props) {
+            super(props);
+            this.state = { width: 0, height: 0 };
+            this.updateWindowDimensions = this.updateWindowDimensions.bind(this);
+          }
+          
+          componentDidMount() {
+            this.updateWindowDimensions();
+            window.addEventListener('resize', this.updateWindowDimensions);
+          }
+          
+          componentWillUnmount() {
+            window.removeEventListener('resize', this.updateWindowDimensions);
+          }
+          
+          updateWindowDimensions() {
+            this.setState({ width: window.innerWidth, height: window.innerHeight });
+          }
+
+        render() {
+            return <ChildComponent height={this.state.height} width={this.state.width}/>;
+        }
+    }
+
+    return PassScreenSize;
+};
